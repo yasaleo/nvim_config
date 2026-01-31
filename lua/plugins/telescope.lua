fdf57e0cb63cc912353f9c75c689a7627fde70aa
yadhu.s <yadhu.s@bluescopetech.com>
2026-01-31 21:09:10 +0530

    tried to show hidden files by default - failed

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,6 +6,12 @@ return {
         hidden = true,
         file_ignore_patterns = {},
       },
+      pickers = {
+        find_files = {
+          hidden = true, -- Show hidden/dot files
+          no_ignore = true, -- Show git-ignored files
+        },
+      },
     },
   },
 }
